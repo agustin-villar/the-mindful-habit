696baacb66f3e22c4d1f01486e7b77d2d588307e
Agustín Villar <avillar@H5731.ad.hugeinc.com>
2017-05-22 01:18:52 -0500

    markup and styles

--- src/js/styles.js
+++ src/js/styles.js
@@ -1,4 +1,11 @@
 // CSS Imports
+import '../scss/fonts.scss';
 import '../scss/variables.scss';
+import '../scss/mixins.scss';
 import '../scss/reset.scss';
 import '../scss/base.scss';
+import '../scss/header.scss';
+import '../scss/job-offer.scss';
+import '../scss/misc-modules.scss';
+import '../scss/application-form.scss';
+import '../scss/footer.scss';
