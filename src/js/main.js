c4be6430f822ab7caa06a482094a4a0e7fb28669
Agustín Villar <avillar@H5731.ad.hugeinc.com>
2017-08-21 22:43:29 -0500

    header styles and markup

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,4 @@
 import './styles.js';
-import TabbedContent from './modules/tabbed-content';
-import MobileMenu from './modules/mobile-menu';
-import Form from './modules/application-form';
 
 document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] +
   ':35729/livereload.js?snipver=1"></' + 'script>');
-
-let myTabs = new TabbedContent();
-myTabs.init();
-
-let mobileMenu = new MobileMenu();
-mobileMenu.init();
-
-let form = new Form();
-form.init();
