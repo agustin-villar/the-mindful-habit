67e8d1f61080cf547d0a60a10115c64de624435a
Agustin <amauro85@gmail.com>
2017-09-17 15:57:51 -0500

    including header and main slide sections

--- src/js/styles.js
+++ src/js/styles.js
@@ -4,4 +4,6 @@ import '../scss/variables.scss';
 import '../scss/mixins.scss';
 import '../scss/reset.scss';
 import '../scss/base.scss';
+import '../scss/common.scss';
 import '../scss/components/_header.scss';
+import '../scss/components/_main-slide.scss';
